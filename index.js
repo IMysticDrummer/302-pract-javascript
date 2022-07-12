cfb0a4826ab4c9d245648f139eed0036ed3c1f0e
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-07-12 22:10:43 +0200

    Primera versión de la ronda de cuartos

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function teamGenerator(teamsNamesArray){
   return teamsObjectsArray
 }
 
+
 /*Primera versión del sorteo del campeonato.
 Devuelve un array con el orden de enfrentamientos.
 En esta primera versión, para cuartos de final */
@@ -30,3 +31,28 @@ console.log(teamsBeforeDraw)
 let teamsAfterDraw=championshipDraw(teamsBeforeDraw)
 console.log('Después sorteo:')
 console.log(teamsAfterDraw)
+
+let winners=[]
+
+while (teamsAfterDraw.length>0) {
+  let team1=teamsAfterDraw.pop()
+  let team2=teamsAfterDraw.pop()
+  let team1Goals=team1.play()
+  let team2Goals=team2.play()
+  //En caso de empate siguen jugando
+  while (team1Goals===team2Goals){
+    team1Goals+=team1.play()
+    team2Goals+=team2.play()
+  }
+
+  if (team1Goals>team2Goals) winners.push(team1)
+  else winners.push(team2)
+  team1.goalsFor+=team1Goals
+  team1.goalsAgainst+=team2Goals
+  team2.goalsFor+=team2Goals
+  team2.goalsAgainst+=team1Goals
+  console.log(`${team1.teamName} ${team1Goals} : ${team2Goals} ${team2.teamName}`)
+}
+
+console.log('Ganadores de los cuartos')
+console.log(winners)
