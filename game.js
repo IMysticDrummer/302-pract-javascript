be2c25147dfd36e9f4b47b2ac5d243724ca0b849
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-07-12 19:16:35 +0200

    Generador de equipos y sorteo
    En game.js se crea una función auxiliar que crea objetos Team
    con los nombres de los equipos declarados.
    Además se crea otra función auxiliar que realiza un sorteo
    de los equipos creados, colocándolos en el orden establecido.
    
    Para la primera parte de la práctica nos servirá para colocar
    a los equipos en los cuartos. Después nos puede servir para
    el sorteo de grupos

--- game.js
+++ game.js
@@ -1,4 +1,32 @@
+const TEAMS=['España', 'Francia', 'Italia', 'Inglaterra', 'Portugal', 'Alemania','Repúblic Checa', 'Bélgica']
+
 const Team=require('./team.js')
 
-let espana= new Team('España')
-console.log(espana.teamName)
+//Generador de equipos
+function teamGenerator(teamsNamesArray){
+  let teamsObjectsArray=[]
+  for (let team of teamsNamesArray) {
+    teamsObjectsArray.push(new Team(team))
+  }
+  return teamsObjectsArray
+}
+
+/*Primera versión del sorteo del campeonato.
+Devuelve un array con el orden de enfrentamientos.
+En esta primera versión, para cuartos de final */
+function championshipDraw (teamsObjectsArray){
+  let vs=[]
+  while (teamsObjectsArray.length>1) {
+    let index=Math.floor(Math.random()*teamsObjectsArray.length)
+    vs=vs.concat(teamsObjectsArray.splice(index,1))
+  }
+  vs.push(teamsObjectsArray.pop())
+  return vs
+}
+
+let teamsBeforeDraw=teamGenerator(TEAMS)
+console.log('Antes sorteo:')
+console.log(teamsBeforeDraw)
+let teamsAfterDraw=championshipDraw(teamsBeforeDraw)
+console.log('Después sorteo:')
+console.log(teamsAfterDraw)
